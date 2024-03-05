1e1d75b506559e28256686bad3e840ee53a6f0d6
Thirumalaiprabu Pushparaj <tpushparaj@microsoft.com>
2024-03-05 21:03:48 +0000

    homework

---
+++ src/com/dsa/problems/arraysandhashing/homeworks/P_13_SetMatrixZeros.java
@@ -0,0 +1,48 @@
+package com.dsa.problems.arraysandhashing.homeworks;
+
+public class P_13_SetMatrixZeros {
+
+    // MEDIUM
+    // Leet code : 73
+    /********************************************************************************************************************
+
+     Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's.
+
+     You must do it in place.
+
+
+     Input: matrix = [
+         [1,1,1],
+         [1,0,1],
+         [1,1,1]
+     ]
+
+     Output: [
+         [1,0,1],
+         [0,0,0],
+         [1,0,1]
+     ]
+
+
+     Input: matrix = [
+         [0,1,2,0],
+         [3,4,5,2],
+         [1,3,1,5]
+     ]
+
+     Output: [
+         [0,0,0,0],
+         [0,4,5,0],
+         [0,3,1,0]
+     ]
+
+     *********************************************************************************************************************/
+
+
+
+
+    public void setZeroes(int[][] matrix) {
+
+    }
+
+}

94ea565d10ab45c0d4f598ec689e1fdd232ba120
Thirumalaiprabu Pushparaj <tpushparaj@microsoft.com>
2024-03-07 22:07:44 +0000

    hw

---
+++ src/com/dsa/problems/arraysandhashing/homeworks/P_15_PairsWithSpecificDifference.java
@@ -0,0 +1,33 @@
+package com.dsa.problems.arraysandhashing.homeworks;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class P_15_PairsWithSpecificDifference {
+
+    // MEDIUM
+    /********************************************************************************************************************
+
+     Given an array arr of distinct integers and a non negative integer k,
+     write a function findPairsWithGivenDifference that returns an array of all pairs [x,y] in arr,
+     such that x - y = k. If no such pairs exist, return an empty array.
+
+     Note: the order of the pairs in the output array should maintain the order of the y element in the original array.
+
+     Examples:
+
+     input:  arr = [0, -1, -2, 2, 1], k = 1
+     output: [[1, 0], [0, -1], [-1, -2], [2, 1]]
+
+     input:  arr = [1, 7, 5, 3, 32, 17, 12], k = 17
+     output: []
+
+     *******************************************************************************************************************/
+
+    static int[][] findPairsWithGivenDifference(int[] arr, int k) {
+        return new int[][]{};
+    }
+
+}

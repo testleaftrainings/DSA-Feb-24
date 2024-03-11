a322d472e5fc51c1dc96f20eaf31d18b7226aaf3
Thirumalaiprabu Pushparaj <tpushparaj@microsoft.com>
2024-03-11 09:33:14 +0000

    calsswork

---
+++ src/com/dsa/problems/p1_arraysandhashing/homeworks/P_16_KSumSubarray.java
@@ -0,0 +1,32 @@
+package com.dsa.problems.p1_arraysandhashing.homeworks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P_16_KSumSubarray {
+
+    // MEDIUM
+    /********************************************************************************************************************
+
+     You're given a list of integers nums. Write a function that
+     returns a boolean representing whether there exists a K-sum subarray of nums.
+
+     A K-sum subarray is any subarray where all of the values add up to K.
+     A subarray is any contiguous section of the array. For the purposes of this
+     problem, a subarray can be as small as one element and as long as the
+     original array.
+
+
+     Sample Input:
+     nums = [-5, -5, 2, 3, -2], K=5
+
+     output: true, Formed by [2,3]
+
+     *******************************************************************************************************************/
+
+
+    public boolean zeroSumSubarray(int[] nums, int k) {
+        return false;
+    }
+
+}
